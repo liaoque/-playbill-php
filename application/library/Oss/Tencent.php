7413fb03b0fab86b28279c94945a351f4aba8137
mzq <mazuqiang@lane.com>
2022-11-11 17:28:01 +0800

    扩展

--- application/library/Oss/Tencent.php
+++ application/library/Oss/Tencent.php
@@ -3,18 +3,39 @@
 namespace Oss;
 
 use Jcupitt\Vips\Image;
+use Qcloud\Cos\Client;
 
 class Tencent implements OssInterface
 {
 
 
+    /**
+     * @var Client
+     */
+    private Client $client;
+
     public function __construct($config)
     {
+        $secretId = $config->get('secretId');
+        $secretKey = $config->get('secretKey');
+        $region = $config->get('region');
+        $bucket = $config->get('bucket');
+
+        $this->client = new Client([
+            'region' => $region,
+            'schema' => 'https',
+            'credentials' => ['secretId' => $secretId, 'secretKey' => $secretKey]
+        ]);
 
     }
 
     public function put(Image $image)
     {
+        $this->client->putObject(array(
+            'Bucket' => $this->bucket,
+            'Key' => $image,
+            'Body' => $image
+        ));
 
     }
 
