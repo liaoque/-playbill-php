2a7735e4826054fc58faed26b2457aec2e9714d0
mzq <mazuqiang@lane.com>
2022-11-26 16:02:30 +0800

    修正多边形组件的宽高

--- application/controllers/Routes.php
+++ application/controllers/Routes.php
@@ -8,6 +8,6 @@ class RoutesController extends \Yaf_Controller_Abstract
     {
         $menu = '{"path":"/permission","meta":{"title":"menus.permission","icon":"lollipop","rank":7},"children":[{"path":"/permission/page/index","name":"PermissionPage","meta":{"title":"menus.permissionPage"}},{"path":"/permission/button/index","name":"PermissionButton","meta":{"title":"menus.permissionButton","authority":[]}}]}';
         $menu = json_decode($menu, true);
-        AppResponse\AppResponse::success([$menu]);
+        AppResponse\AppResponse::success([]);
     }
 }
